4c29438c587f7a1f659210b0ef552ee7d2014322
Nour <programmer.nour@gmail.com>
2017-01-22 12:21:17 +0400

    Add the first version of Movies Web Api controller along with the db context to connect to the database.

---
+++ Iconic/Models/IconicContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Iconic.Models
+{
+    public class IconicContext : DbContext
+    {
+
+
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<Location> Locations { get; set; }
+    }
+}
