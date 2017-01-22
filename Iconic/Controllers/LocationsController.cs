e437092e35ed1881ba5419eaac9a8443a0665254
Nour <programmer.nour@gmail.com>
2017-01-22 13:30:56 +0400

    Adding locations controller with the basic method actions. Delete redundant values controller Add location view model to be sent in the response

---
+++ Iconic/Controllers/LocationsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Iconic.Models;
+using System.Web;
+using System.IO;
+
+namespace Iconic.Controllers
+{
+    public class LocationsController : ApiController
+    {
+        private IconicDbContext db = new IconicDbContext();
+
+        private const int pageSize = 20;
+
+        // GET: api/Locations
+        public IEnumerable<LocationViewModel> GetLocations(int page = 1)
+        {
+            var list = db.Locations.OrderBy(o => o.Id).Skip((page - 1) * pageSize).Take(pageSize)
+                .Select(s => new LocationViewModel() { Id = s.Id, Name = s.Name, Description = s.Description }).ToList();
+            list.ForEach(f => f.Image = Url.Route("DefaultApi", new { controller = "Locations", id = f.Id }));
+            return list;
+        }
+
+        // GET: api/Locations/5
+        [ResponseType(typeof(Location))]
+        public async Task<IHttpActionResult> GetLocation(int id)
+        {
+            Location location = await db.Locations.FindAsync(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(location);
+        }
+        
+        // PUT: api/Locations/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutLocation(int id, Location location)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != location.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(location).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LocationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Locations
+        [ResponseType(typeof(Location))]
+        public async Task<IHttpActionResult> PostLocation(Location location)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Locations.Add(location);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = location.Id }, location);
+        }
+
+        // DELETE: api/Locations/5
+        [ResponseType(typeof(Location))]
+        public async Task<IHttpActionResult> DeleteLocation(int id)
+        {
+            Location location = await db.Locations.FindAsync(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            db.Locations.Remove(location);
+            await db.SaveChangesAsync();
+
+            return Ok(location);
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LocationExists(int id)
+        {
+            return db.Locations.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ Iconic/Models/LocationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Iconic.Models
+{
+    public class LocationViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+    }
+}
