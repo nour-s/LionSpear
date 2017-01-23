7c84a108ea3e4e2e4bdc11fe08de406239da00e5
Nour <programmer.nour@gmail.com>
2017-01-24 03:19:05 +0400

    Create the class diagram of the db classes Arrange the view mode classes in their folder.

--- Iconic/Models/BucketListLocation.cs
+++ Iconic/Models/BucketListLocation.cs
@@ -5,6 +5,10 @@ using System.Web;
 
 namespace Iconic.Models
 {
+    /// <summary>
+    /// Represent a location that is in a bucket list of a user. it is a joining class between the user and the location.
+    /// It contains information about this item of a bucket list.
+    /// </summary>
     public class BucketListLocation
     {
         public int Id { get; set; }
